f5a5a0563d18b4a60f23b8a3ff9a217795d5f300
Petta <petta07@gmail.com>
2021-06-22 22:53:18 +0300

    add checkout page data

--- store/views.py
+++ store/views.py
@@ -19,17 +19,26 @@ def cart(request):
         # создаю пока пустую корзину для не вошедшего в систему пользователя
 
         items = []
-    # Cоздаю объект для пользователей, не вошедших в систему. Для этого объекта добавляю атрибуты get_cart_total
-    # и get_cart_items и устанавливаю их равными 0, поскольку корзина гостей сейчас пуста.
-    # Представляю порядок действий пользователя с помощью словаря, чтобы в нашем шаблоне всегда было
-    # что запросить когда у нас есть покупатель и заказ / корзина, запрашиваю элементы корзины
-    # с помощью order.orderitem_set.all ()
+        # Cоздаю объект для пользователей, не вошедших в систему. Для этого объекта добавляю атрибуты get_cart_total
+        # и get_cart_items и устанавливаю их равными 0, поскольку корзина гостей сейчас пуста.
         order = {'get_cart_total': 0, 'get_cart_items': 0}
 
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
+# Представляю порядок действий пользователя с помощью словаря, чтобы в нашем шаблоне всегда было
+# что запросить когда у нас есть покупатель и заказ / корзина, запрашиваю элементы корзины
+# с помощью order.orderitem_set.all ()
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
