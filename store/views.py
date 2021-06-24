886d6a70549ebe27594e5bc5f58a594cc0f2a414
Petta <petta07@gmail.com>
2021-06-24 16:27:24 +0300

    shipping information, product does not exist

--- store/views.py
+++ store/views.py
@@ -49,31 +49,48 @@ def cart(request):
 
         # чтобы посмотреть общее количество товаров в корзине, мы можем просто просмотреть наш словарь корзины
         # и добавить общее количество каждого товара
-        #  далее добавим цикл в нашу «корзину» и запросим продукт, чтобы получить цену
+
+        # Далее добавим цикл в нашу «корзину» и запросим продукт, чтобы получить цену
+
+        # Потенциальная проблема, с которой мы можем столкнуться, заключается в том, что пользователь добавляет товар
+        # в свою корзину и за время, необходимое пользователю для оформления заказа, допустим, администратор удаляет товар
+        # из базы данных. Это означает, что мы будем запрашивать продукт с не верным идентификатором.
+        # Мы можем исправить это, добавив в наш цикл блок «try / except» и проигнорировав эту проблему.
         for i in cart:
-            cartItems += cart[i]['quantity']
+            try:
+                cartItems += cart[i]['quantity']
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
-            # На каждой итерации цикла для товаров в нашей корзине мы будем создавать объект item и добавлять его
-            # в наш список товаров. Объект item будет содержать все те же атрибуты, что и наша модель OrderItem.
-            item = {
-                'product': {
-                    'id': product.id,
-                    'name': product.name,
-                    'price': product.price,
-                    'imageURL': product.imageURL
-                },
-                'quantity': cart[i]['quantity'],
-                'get_total': total,
-            }
-            items.append(item)
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
+                # На каждой итерации цикла для товаров в нашей корзине мы будем создавать объект item и добавлять его
+                # в наш список товаров. Объект item будет содержать все те же атрибуты, что и наша модель OrderItem.
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL
+                    },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total,
+                }
+                items.append(item)
+
+                # запросим атрибут «digital» продуктов и Django значение «shipping», чтобы проверить,
+                # является ли один из элементов НЕ цифровым элементом
+
+                if product.digital == False:
+                    order['shipping'] = True
+
+            except:
+                pass
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
+
 
 # Представляю порядок действий пользователя с помощью словаря, чтобы в нашем шаблоне всегда было
 # что запросить когда у нас есть покупатель и заказ / корзина, запрашиваю элементы корзины
