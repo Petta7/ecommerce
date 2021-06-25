cc2ef5e1e1448a6126c9fd5a6b830f0f8b06323c
Petta <petta07@gmail.com>
2021-06-25 16:06:22 +0300

    add utils.py, copy cart logic, user cookieCart in views

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ import json
 import datetime
 
 from .models import *
+from .utils import cookieCart
 
 
 # импортирую все модели и запрашиваю продукты в представлении магазина
@@ -14,9 +15,8 @@ def store(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
 
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
@@ -30,63 +30,12 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        #  добавим логику в представление "cart" в нашем условном выражении, чтобы мы получали данные из файлов cookie
-        #  только тогда, когда посетитель НЕ вошел в систему. С помощью оператора try / except создаем пустую корзину
-        #  для работы, если она не существует
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-        except:
-            cart = {}
-        print('Cart:', cart)
-
-        # создаю пока пустую корзину для не вошедшего в систему пользователя
-
-        items = []
-        # Cоздаю объект для пользователей, не вошедших в систему. Для этого объекта добавляю атрибуты get_cart_total
-        # и get_cart_items и устанавливаю их равными 0, поскольку корзина гостей сейчас пуста.
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
-
-        # чтобы посмотреть общее количество товаров в корзине, мы можем просто просмотреть наш словарь корзины
-        # и добавить общее количество каждого товара
-
-        # Далее добавим цикл в нашу «корзину» и запросим продукт, чтобы получить цену
-
-        # Потенциальная проблема, с которой мы можем столкнуться, заключается в том, что пользователь добавляет товар
-        # в свою корзину и за время, необходимое пользователю для оформления заказа, допустим, администратор удаляет товар
-        # из базы данных. Это означает, что мы будем запрашивать продукт с не верным идентификатором.
-        # Мы можем исправить это, добавив в наш цикл блок «try / except» и проигнорировав эту проблему.
-        for i in cart:
-            try:
-                cartItems += cart[i]['quantity']
-
-                product = Product.objects.get(id=i)
-                total = (product.price * cart[i]['quantity'])
-
-                order['get_cart_total'] += total
-                order['get_cart_items'] += cart[i]['quantity']
-                # На каждой итерации цикла для товаров в нашей корзине мы будем создавать объект item и добавлять его
-                # в наш список товаров. Объект item будет содержать все те же атрибуты, что и наша модель OrderItem.
-                item = {
-                    'product': {
-                        'id': product.id,
-                        'name': product.name,
-                        'price': product.price,
-                        'imageURL': product.imageURL
-                    },
-                    'quantity': cart[i]['quantity'],
-                    'get_total': total,
-                }
-                items.append(item)
-
-                # запросим атрибут «digital» продуктов и Django значение «shipping», чтобы проверить,
-                # является ли один из элементов НЕ цифровым элементом
-
-                if product.digital == False:
-                    order['shipping'] = True
-
-            except:
-                pass
+        # Очистим всю логику и вызовем нашу функцию cookieCart. Устанавливаем возвращаемое значение в переменную cookieData.
+        # Затем мы можем получить доступ к этим значениям из cookieData и получить то, что нам нужно для каждого представления.
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
@@ -103,9 +52,11 @@ def checkout(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
