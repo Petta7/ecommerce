4cd8c448d99ee6bacc6c478d5796ac4e59620787
Petta <petta07@gmail.com>
2021-07-06 18:01:52 +0300

    delete gitignore

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from secrets import SECRET_KEY
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-0wol0^vy8#*u5dv)zrdgjc$api6=njiljlxc!(5bz3s^n((4+b'
+SECRET_KEY = 'django-insecure-0wol0^vy8#*u5dv)zrdgjc$api6=njiljlxc!(5bz3s^n((4+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
