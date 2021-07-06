de65bff7fd7affafd83c7453a6bb30eedd96ace9
Petta <petta07@gmail.com>
2021-07-06 17:22:45 +0300

    fixed the error in gitignore file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from .secrets import SECRET_KEY
+from secrets import SECRET_KEY
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-0wol0^vy8#*u5dv)zrdgjc$api6=njiljlxc!(5bz3s^n((4+b'
 
